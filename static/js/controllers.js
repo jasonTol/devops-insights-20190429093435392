1bdba11a420fcf8779a1e5370694b4f03f925989
jason tollison <jrt37@students.waikato.ac.nz>
2019-05-11 10:26:51 +0000

    Tried to an angular Dependency Injection. Displays a string

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,4 +1,83 @@
 
+var ConsoleModule = angular.module('ConsoleModule', ['ngRoute', 'googMap']);
+
+ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
+    function ($routeProvider, $locationProvider, $sceDelegateProvider, $httpProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/partials/Byzip.html',
+        controller: 'wcontroller',
+        controllerAs: 'wcontroller'
+    });
+}]);
+
+ConsoleModule.controller('wcontroller', ['createMap', '$scope', '$http', '$routeParams', '$timeout', '$sce',
+    function($scope, $http, $routeParams, $timeout, $sce, createMap) {
+
+    $scope.somemessage = "Some weather";
+    $scope.zip1City = "";
+    $scope.zip1Weather = "";
+
+    $scope.zip = function(which) {
+
+        var data = "";
+        //Checks ng-model value from Byzip.html
+        if(which === 1) {
+            data = $scope.zip1m;
+        } else if(which === 2) {
+            data = $scope.zip2m;
+        } else if(which === 3) {
+            data = $scope.zip3m;
+        } else if(which === 4) {
+            data = $scope.zip4m;
+        }
+
+        //if(data.length === 5) { //Sends city from ng-change value and outputs response to user
+        if(data.length > 2) { //Sends city from ng-change value and outputs response to user
+            $http({
+                method: "GET",
+                url: '/api/v1/getWeather?city=' + data
+            }).then( function(response) {
+                if(which === 1) {
+                    $scope.zip1City = response.data.city;
+                    $scope.zip1Weather = response.data.weather;
+                    $scope.zip1lat = response.data.lat;
+                    $scope.zip1lon = response.data.lon;
+                    window.alert(createMap.somteen);
+                } else if(which === 2) {
+                    $scope.zip2City = response.data.city;
+                    $scope.zip2Weather = response.data.weather;
+                } else if(which === 3) {
+                    $scope.zip3City = response.data.city;
+                    $scope.zip3Weather = response.data.weather;
+                } else if(which === 4) {
+                    $scope.zip4City = response.data.city;
+                    $scope.zip4Weather = response.data.weather;
+                } 
+            });
+        } else {
+            if(which === 1) {
+                    $scope.zip1City = "";
+                    $scope.zip1Weather = "";
+                    $scope.zip1lat = "";
+                    $scope.zip1lon = "";
+                } else if(which === 2) {
+                    $scope.zip2City = "";
+                    $scope.zip2Weather = "";
+                } else if(which === 3) {
+                    $scope.zip3City = "";
+                    $scope.zip3Weather = "";
+                } else if(which === 4) {
+                    $scope.zip4City = "";
+                    $scope.zip4Weather = "";
+                } 
+        }
+    };
+    
+}]);
+
+
+/* --------{ Before changes }---------
+
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
 
 ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
@@ -71,3 +150,4 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     };
     
 }]);
+*/
--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,22 @@
+
+angular.module('googMap', [])
+.factory('createMap', function() {
+  /*var currencies = ['USD', 'EUR', 'CNY'];
+  var usdToForeignRates = {
+    USD: 1,
+    EUR: 0.74,
+    CNY: 6.09
+  }; */
+  var displaySomething = function() {
+    return "If it is nobler in mind to suffer the slings and arrows of outragious fortune, or take arms against a sea of troubles";
+  };
+
+  return {
+    somteen: displaySomething
+  };
+});
+
+/*
 var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -5,3 +24,4 @@ var map;
       zoom: 8
     });
   }
+  */
