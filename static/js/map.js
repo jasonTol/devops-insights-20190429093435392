e7a27215bf2e3478f4e1b441a9c85880277e78a0
jason tollison <jrt37@students.waikato.ac.nz>
2019-05-12 20:53:43 +0000

    Added listener to the map for clicks, which places markers. Tidied up some comments.

--- static/js/map.js
+++ static/js/map.js
@@ -1,26 +1,37 @@
 
 angular.module('googMap', [])
 .factory('createMap', function() {
-	var map;
   var markerArray = [0,0,0,0];
   
   var initialiseMap = function(mapDOM){
   	if(mapDOM == null){
 		mapDOM = new google.maps.Map(document.getElementById('map'), {
 	      center: {lat: -39.0363124, lng: 177.4120491},
-	      zoom: 5
+	      zoom: 6
+	    });
+	    
+	    mapDOM.addListener('click', function(e){
+	    	placeMarkerAndPanTo(e.latLng, mapDOM);
 	    });
 	    
 	    return mapDOM;
 	}
   };
   
+  function placeMarkerAndPanTo(latLng, map){ //Place marker on map and pan to that location
+  	var marker = new google.maps.Marker({
+  		position: latLng,
+  		map: map
+  	});
+  	map.panTo(latLng);
+  }
+  
  
   var updateMarker = function(index, gooMap, city, lat, lon)
   {
     var myLatLng = new google.maps.LatLng(lat, lon);
     
-	console.log("Inside the updateMarker() of createMap service --{lat = " + lat + " lon = " + lon + " index = " + index + "}--");
+	//console.log("Inside the updateMarker() of createMap service --{lat = " + lat + " lon = " + lon + " index = " + index + "}--");
     	
 	var marker = new google.maps.Marker({
    		position: myLatLng,
@@ -28,14 +39,12 @@ angular.module('googMap', [])
 	    title: city
   	});
   	
-  	console.log("type of variable in the array = " + typeof markerArray[index-1]);
-  	if(markerArray[index-1] !== 0){
-  		console.log("first array item " + markerArray[index-1].getTitle());
+  	if(markerArray[index-1] !== 0){ //If there is a marker in the array at index clear it
+  		//console.log("first array item " + markerArray[index-1].getTitle());
 		markerArray[index-1].setMap(null); //Remove the marker for this position if it exists
   	}
   	
-  	markerArray[index-1] = marker; //Add marker to array at this position
-  	console.log("first array item " + markerArray[index-1].getTitle());
+  	markerArray[index-1] = marker; //Add marker to array at pass in index
   	return marker;
   };
 
