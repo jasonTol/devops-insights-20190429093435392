ca2839ca6df84eec6ba4c73afbac7ca82469cc24
jason tollison <jrt37@students.waikato.ac.nz>
2019-05-14 07:24:05 +0000

    Updated apiv1.js to search for weather using geolocation data

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -8,10 +8,67 @@ var request = REQUEST.defaults( {
 });
 
 var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
+var aurl;
+exports.getWeather = function(req, res) {
+	var newCity = req.query.city;
+	if( (newCity === null) || (typeof(newCity) === 'undefined') ) {
+		var newLat = req.query.lat;
+		var newLng = req.query.lng;
+		if(((newLat === null) || (newLng === null)) || ((typeof(newLat) === 'undefined') || (typeof(newLng) === 'undefined'))){
+			return res.status(400).send('city missing');
+		}
+		
+		aurl = OPENWEATHERURL + '&lat=' + newLat + '&lon=' + newLng;
+		
+	}
+	else{
+		aurl = OPENWEATHERURL + '&q=' + newCity + ',NZ';
+	}
+	
+	
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var response = {city: body.name, weather: weath, lat: body.coord.lat, lon: body.coord.lon};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather', exports.getWeather);
+
+
+
+
+/*
+var express = require('express');
+var router = express.Router();
+var REQUEST = require('request');
+
+var request = REQUEST.defaults( {
+    strictSSL: false
+});
+
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
 
 exports.getWeather = function(req, res) {
 	var newCity = req.query.city;
 	if( (newCity === null) || (typeof(newCity) === 'undefined') ) {
+		var newLat = req.query.lat;
+		var newLng = req.query.lng;
+		if(((newLat === null) || (newLng === null)) || ((typeof(newLat) === 'undefined') || (typeof(newLng) === 'undefined')))
 		return res.status(400).send('city missing');
 	}
 	
@@ -37,7 +94,8 @@ exports.getWeather = function(req, res) {
     });
 
 };
-router.get('/getWeather', exports.getWeather);
+*/
+
 
 /*
 exports.getWeather2 = function(req, res) {
