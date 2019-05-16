e4ad8cbfc73d66c2f8e2aad925336a1504e5a1a8
jason tollison <jrt37@students.waikato.ac.nz>
2019-05-16 09:51:00 +0000

    Making an attempt at database connection

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -110,6 +110,23 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
 	  
 	initialiseMap(); //Initialise the googlemap
 	
+	function getFromDb(){
+		var ibmdb = require('ibm_db');
+		 
+		ibmdb.open("DATABASE=<dbname>;HOSTNAME=<myhost>;UID=db2user;PWD=password;PORT=<dbport>;PROTOCOL=TCPIP", function (err,conn) {
+		  if (err) return console.log(err);
+		  
+		  conn.query('select 1 from sysibm.sysdummy1', function (err, data) {
+		    if (err) console.log(err);
+		    else console.log(data);
+		 
+		    conn.close(function () {
+		      console.log('done');
+		    });
+		  });
+		});
+	}
+	
     $scope.somemessage = "Some weather";
     $scope.zip1City = "";
     $scope.zip1Weather = "";
